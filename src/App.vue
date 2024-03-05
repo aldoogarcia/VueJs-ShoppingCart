3b10cd5ec718850d40b4005171df48dd455348de
aldo garcia <aldoredcreo@gmail.com>
2024-03-04 23:34:33 -0600

    ⭐ Feat list rendering add a list of items using v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,18 @@
 <script setup>
-
+import { ref } from 'vue'
+const items = ref([
+  { id: 1, label: "Leche" }, // Unique IDs and capitalized label
+  { id: 2, label: "Carne" },
+  { id: 3, label: "Arroz" },
+  { id: 3, label: "Pezcado" }
+]);
 </script>
 
 <template>
-  <h1>app</h1>
+  <ul>
+    <li v-for="item in items" :key="item.id"> {{ item.label }}
+    </li>
+  </ul>
 </template>
+
 
