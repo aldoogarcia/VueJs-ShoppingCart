5a0a5630c69ae5157f14c12f848cde8c16c6b7d1
aldo garcia <aldoredcreo@gmail.com>
2024-03-11 22:07:24 -0600

    ⭐Feat eventos de usuario crea el boton para agragar nuevos items a la lista

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,32 @@
+<!-- eslint-disable vue/no-parsing-error -->
 <script setup>
 import { ref } from 'vue'
+
 const items = ref([
   { id: 1, label: "Leche" }, // Unique IDs and capitalized label
   { id: 2, label: "Carne" },
   { id: 3, label: "Arroz" },
-  { id: 3, label: "Pezcado" }
+  { id: 4, label: "Pezcado" }
 ]);
+
+const newItem= ref("");
+
+
 </script>
 
 <template>
+  <h1>🛒 App lista de compras</h1>
+  <div class="add-item form">
+    <input v-model="newItem" type="text" placeholder="Agregar articulo">
+    <label><input type="checkbox" v-model="newItem">Alta prioridad</label>
+    <br>
+    <!--Buton-->
+    <button class="btn btn-primary" v-on:click="items.push({id: items.length, label:newItem})">Agregar articulo</button>
+  </div>
+  <!--Checkbox-->
+
   <ul>
-    <li v-for="item in items" :key="item.id"> {{ item.label }}
+    <li v-for="({id,label},i)  in items" v-bind:key="id">🛒 {{ label }} 
     </li>
   </ul>
 </template>
