29028854966e88fe9330617583725eb486e55f30
aldo garcia <aldoredcreo@gmail.com>
2024-03-18 19:34:13 -0600

    ⭐FEAT: v-else

--- src/App.vue
+++ src/App.vue
@@ -3,10 +3,10 @@
 import { ref } from 'vue'
 
 const items = ref([
-  { id: 1, label: "Leche" }, // Unique IDs and capitalized label
-  { id: 2, label: "Carne" },
-  { id: 3, label: "Arroz" },
-  { id: 4, label: "Pezcado" }
+  //{ id: 1, label: "Leche" }, // Unique IDs and capitalized label
+  //{ id: 2, label: "Carne" },
+  //{ id: 3, label: "Arroz" },
+  //{ id: 4, label: "Pezcado" }
 ]);
 
 const newItem= ref("");
@@ -18,24 +18,38 @@ const saveItemp = ()=> {
   newItem.value= "";
 };
 
+const mostrar = ref(false);
+const mostrarOcultar=(condicion)=>{
+  mostrar.value=condicion;
+}
 </script>
+
 <template>
-<form v-on:submit.prevent="saveItemp" class="add-item form">
-  <h1>🛒 App lista de compras</h1>
+  <!-- head -->
+  <div class="header">
+    <h1>
+      🛒 App lista de compras
+    </h1>
+    <button v-on:click="mostrarOcultar(false)" v-if="mostrar" class="btn">Cancelar</button>
+    <button v-on:click="mostrarOcultar(true)" v-else class="btn btn-primary">Agregar</button>
+  </div>
+ 
+  <!-- Formulario -->
+<form v-if="mostrar" v-on:submit.prevent="saveItemp" class="add-item form">
+  
   <div class="add-item form">
     <input  v-model="newItem" type="text" placeholder="Agregar articulo">
     <label><input type="checkbox" v-model="newItem">Alta prioridad</label>
-    <br>
     <!--Buton-->
     <button class="btn btn-primary" >Agregar articulo</button>
   </div>
   <!--Checkbox-->
 </form>
+<!--Entrega de lista  -->
 <ul>
   <li v-for="({id,label},i)  in items" v-bind:key="id">🛒 {{ label }} 
   </li>
 </ul>
-
+<!-- Mensaje condicional -->
+<p v-if="items.length===0">No hay elementos en la lista 🥀</p>
 </template>
-
-
