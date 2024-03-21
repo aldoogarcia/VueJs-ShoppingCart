4505f018c373aaece88c58f76054eb16d1ef47cf
aldo garcia <aldoredcreo@gmail.com>
2024-03-21 12:07:19 -0600

    ⭐FEAT : Purchased

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,14 @@
 import { ref } from 'vue'
 
 const items = ref([
-  { id: 1, label: "Leche",purchased:false,highPriority:true }, // Unique IDs and capitalized label
-  { id: 2, label: "Carne",purchased:true ,highPriority:false},
-  { id: 3, label: "Arroz",purchased:false,highPriority:true },
-  { id: 4, label: "Pezcado",purchased:false,highPriority:false }
+   { id: 1, label: "Leche",purchased:false,highPriority:true }, // Unique IDs and capitalized label
+   { id: 2, label: "Carne",purchased:true ,highPriority:false},
+   { id: 3, label: "Arroz",purchased:false,highPriority:true },
+   { id: 4, label: "Pezcado",purchased:false,highPriority:false }
 ]);
+const togglePurchased=(item)=>{
+  item.purchased=!item.purchased;
+}
 const header=ref("Lista de compras");
 const newItem= ref("");
 
@@ -22,6 +25,8 @@ const mostrar = ref(false);
 const mostrarOcultar=(condicion)=>{
   mostrar.value=condicion;
 }
+
+
 </script>
 
 <template>
@@ -48,10 +53,17 @@ const mostrarOcultar=(condicion)=>{
 <!--Entrega de lista  -->
 <ul>
   <li v-for="({id,label,purchased,highPriority},i)  in items"
+  @click="togglePurchased(items[i])"
   :class="{priority:highPriority, strikeout:purchased}"
   v-bind:key="id">🛒 {{ label }} 
   </li>
 </ul>
+<!-- <ul>
+  <li v-for="({id,label,purchased,highPriority},i)  in items"
+  :class="[purchased?'strikeout':'',highPriority?'priority':'']"
+  v-bind:key="id">🛒 {{ label }} 
+  </li>
+</ul> -->
 <!-- Mensaje condicional -->
 <p v-if="items.length===0">No hay elementos en la lista 🥀</p>
 </template>
